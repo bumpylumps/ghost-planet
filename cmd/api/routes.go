4462cc36a38e46161d24088247691c1fc4ce9abf
Alex Fulop <bumpsites@gmail.com>
2025-09-06 13:33:36 -0600

    typo test

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,11 +9,15 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// api routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/investigations", app.createInvestigationHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/investigations/:id", app.showInvestigationHandler)
 
 	//return that dang ol httprouter instance
-	return router
+	return routers
 }
